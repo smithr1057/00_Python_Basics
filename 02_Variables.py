1d8fa4ac157f46809e777257637972e3481a3f05
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2022-08-05 10:35:02 +1200

    Create 02_Variables.py

---
+++ 02_Variables.py
@@ -0,0 +1,11 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "Hello World"
+
+# they can also be numbers
+fav_num = 11
+print(fav_num * 2)
+
+num_string = "5"
+print(num_string * 5)
